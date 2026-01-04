8a8fd77542410b4c42ae03df1ef6088fe4d57b37
김윤성 <c335098@g.hongik.ac.kr>
2026-01-04 17:41:47 +0900

    fix: 안쓰는 파일 정리

--- storix-fe/src/app/home/page.tsx
+++ storix-fe/src/app/home/page.tsx
@@ -3,32 +3,32 @@
 
 import { useState } from 'react'
 import HomeHeader from '@/components/home/HomeHeader'
-import TopicNav from '@/components/home/todayTopicRoom/TopicNav'
+import { CardNav } from '@/components/home/todayTopicRoom/CardNav'
 import { TopicRoomCoverSlider } from '@/components/home/todayTopicRoom/TopicRoomCoverSlider'
 import { TopicRoomData } from '@/components/home/todayTopicRoom/TopicroomCoverCard'
 import HotFeedSlider from '@/components/home/hotFeed/HotFeedSlider'
-import MyTypeSlider from '@/components/home/myType/MyTypeSlider'
+import MyTypeCard from '@/components/home/myType/MyTypeCard'
 import HashtagList from '@/components/common/HashtagList'
 import NavBar from '@/components/common/NavBar'
 
 const MOCK_ROOMS: TopicRoomData[] = [
   {
     id: 'room1',
-    imageUrl: '/sample/topicroom-1.jpg', // 일단 public에 더미 이미지 넣어두고 쓰기
+    imageUrl: '/sample/topicroom-1.webp', // 일단 public에 더미 이미지 넣어두고 쓰기
     title: '탄서방 시어머니회',
     subtitle: '웹툰 상수리나무 아래',
     memberCount: 13,
   },
   {
     id: 'room2',
-    imageUrl: '/sample/topicroom-1.jpg',
+    imageUrl: '/sample/topicroom-1.webp',
     title: '탄서방 시어머니회 2',
     subtitle: '웹툰 상수리나무 아래',
     memberCount: 20,
   },
   {
     id: 'room3',
-    imageUrl: '/sample/topicroom-1.jpg',
+    imageUrl: '/sample/topicroom-1.webp',
     title: '탄서방 시어머니회 3',
     subtitle: '웹툰 상수리나무 아래',
     memberCount: 22,
@@ -47,27 +47,24 @@ export default function Home() {
       <HomeHeader />
       <div className="w-full h-full flex flex-col pb-32">
         {/* 위쪽만 스크롤 */}
-        <div className="flex-1 overflow-y-auto overflow-x-hidden">
+        <div className="flex-1 flex flex-col gap-[24px] overflow-y-auto overflow-x-hidden">
           <div className="flex flex-col items-center text-center">
-            <TopicNav />
+            <CardNav header="실시간 작품 이야기!" roomName="topicroom" />
             <TopicRoomCoverSlider rooms={MOCK_ROOMS} />
           </div>
+
           <div className="flex flex-col w-full">
-            <p className="flex items-center h-17 text-left heading-1 px-1">
-              취향 저격 작품 탐색
-            </p>
-            <MyTypeSlider />
+            <CardNav header="이 작품, 내 취향일까?" roomName="#" />
+            <MyTypeCard />
           </div>
+
           <div className="flex flex-col w-full">
-            <p className="flex items-center h-17 text-left heading-1 px-1">
-              지금 뜨는 글
-            </p>
+            <CardNav header="오늘의 피드" roomName="#" />
             <HotFeedSlider />
           </div>
+
           <div className="flex flex-col w-full mb-36">
-            <p className="flex items-center h-17 text-left heading-1 px-1">
-              이런 해시태그는 어때요?
-            </p>
+            <CardNav header="이런 키워드, 좋아하실 것 같아요" roomName="#" />
             <HashtagList />
           </div>
         </div>
--- storix-fe/src/components/home/myType/MyTypeCard.tsx
+++ storix-fe/src/components/home/myType/MyTypeCard.tsx
@@ -1,27 +1,17 @@
 // src/components/home/myType/MyTypeCard.tsx
-'use client'
+import Image from 'next/image'
 
-// 카드 데이터 타입 이후에 반영될 예정
-export interface HotFeedData {
-  id: string
-  author: string
-  title: string
-  content: string
-  likeCount: number
-  commentCount: number
-}
-
-type MyTypeCardProps = {
-  className?: string
-}
-
-/* 지금 뜨는 글 카드 - 내용은 비워두고 레이아웃/사이즈만 잡은 버전 */
-export const MyTypeCard = ({ className }: MyTypeCardProps) => {
+export default function MyTypeCard() {
   return (
     <div
-      className={`flex w-[112px] h-[208px] rounded-2xl shadow-sm flex-shrink-0 ${
-        className ?? 'bg-white'
-      }`}
-    ></div>
+      className={`relative flex w-[353px] h-[173px] rounded-2xl shadow-sm flex-shrink-0 bg-gray-100`}
+    >
+      <Image
+        src={'/sample/card_serach_insidecover.webp'}
+        alt={'으아아앙'}
+        fill
+        className="object-cover"
+      />
+    </div>
   )
 }
