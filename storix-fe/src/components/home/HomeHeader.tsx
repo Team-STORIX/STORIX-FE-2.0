1054311de88343d33f93809585bd57e4f4d1ff33
김윤성 <c335098@g.hongik.ac.kr>
2026-01-23 00:04:53 +0900

    ..Test

--- storix-fe/src/components/home/HomeHeader.tsx
+++ storix-fe/src/components/home/HomeHeader.tsx
@@ -4,7 +4,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-/** 상단 로고 + 검색 + 알림 헤더 */
+/* 상단 로고 + 검색 + 알림 헤더 */
 export default function HomeHeader() {
   return (
     <header className="flex py-4 items-center justify-between">
