16809768148bfc5fb5b74867553e34dbce01ccda
codus02 <chaetree02@gmail.com>
2026-04-13 23:50:42 +0900

    onboarding design fix

---
+++ storix-fe/android/app/build/intermediates/assets/debug/mergeDebugAssets/public/icons/layout/FowardArrowIcon.tsx
@@ -0,0 +1,16 @@
+export default function ForwardArrowIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M21.6 11.9999L17 7.39994L18.0537 6.34619L23.7075 11.9999L18.0537 17.6537L17 16.5999L21.6 11.9999Z"
+        fill="#100F0F"
+      />
+    </svg>
+  )
+}
