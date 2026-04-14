70f345bbc8459658f14e51422c9c79802826a96f
codus02 <chaetree02@gmail.com>
2026-04-15 00:24:27 +0900

    fix: remove outputFileTracingRoot

--- storix-fe/next.config.ts
+++ storix-fe/next.config.ts
@@ -2,7 +2,7 @@ import path from 'node:path'
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.resolve(__dirname),
+  //outputFileTracingRoot: path.resolve(__dirname),
   async headers() {
     return [
       {
